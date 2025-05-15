363b98af5809ff64dfd47e88cfed2ff49afb6c3a
tikt98028 <tikt98028@gmail.com>
2025-05-15 17:48:16 +0300

    main.py

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, ChatJoinRequestHandler, ContextTypes
 from flask import Flask
 from threading import Thread
-import os
+import asyncio
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = "7690355390:AAE4uas_oqNBCjpwJdtapH1tqNqWbU3VjjU"
 WELCOME_MESSAGE = "Привіт 👋 Дякую, що приєднався до нашого каналу! Якщо є питання — напиши сюди."
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "I am alive!"
 
 async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.chat_join_request.from_user
@@ -16,18 +22,17 @@ async def approve(update: Update, context: ContextTypes.DEFAULT_TYPE):
     except:
         print(f"❌ Не вдалося написати @{user.username}")
 
+def run_flask():
+    app.run(host="0.0.0.0", port=10000)
+
 def run_telegram_bot():
+    asyncio.run(start_bot())
+
+async def start_bot():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(ChatJoinRequestHandler(approve))
+    await app.run_polling()
 
-flask_app = Flask(__name__)
-
-@flask_app.route('/')
-def home():
-    return 'I am alive!'
-
-def run_flask():
-    flask_app.run(host='0.0.0.0', port=10000)
-
-Thread(target=run_telegram_bot).start()
-Thread(target=run_flask).start()
+if __name__ == "__main__":
+    Thread(target=run_flask).start()
+    run_telegram_bot()
